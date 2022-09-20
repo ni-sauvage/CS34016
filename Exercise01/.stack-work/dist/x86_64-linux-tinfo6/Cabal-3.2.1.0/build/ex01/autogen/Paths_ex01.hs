60136ef18d61bd7c653564c8d705c20fadbee931
Niall Sauvage <sauvagen@tcd.ie>
2022-09-20 14:16:05 +0100

    Added Ex01 to repo

---
+++ Exercise01/.stack-work/dist/x86_64-linux-tinfo6/Cabal-3.2.1.0/build/ex01/autogen/Paths_ex01.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+module Paths_ex01 (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/home/sauvagen/CS34016/Exercise01/.stack-work/install/x86_64-linux-tinfo6/3c68f6377a3187d2f8c1475b9b6fab501756f8d123a6e1be6d551a7060561537/8.10.7/bin"
+libdir     = "/home/sauvagen/CS34016/Exercise01/.stack-work/install/x86_64-linux-tinfo6/3c68f6377a3187d2f8c1475b9b6fab501756f8d123a6e1be6d551a7060561537/8.10.7/lib/x86_64-linux-ghc-8.10.7/ex01-0.1.0.0-1EJzuwZCEhFA2HR1x76Rws-ex01"
+dynlibdir  = "/home/sauvagen/CS34016/Exercise01/.stack-work/install/x86_64-linux-tinfo6/3c68f6377a3187d2f8c1475b9b6fab501756f8d123a6e1be6d551a7060561537/8.10.7/lib/x86_64-linux-ghc-8.10.7"
+datadir    = "/home/sauvagen/CS34016/Exercise01/.stack-work/install/x86_64-linux-tinfo6/3c68f6377a3187d2f8c1475b9b6fab501756f8d123a6e1be6d551a7060561537/8.10.7/share/x86_64-linux-ghc-8.10.7/ex01-0.1.0.0"
+libexecdir = "/home/sauvagen/CS34016/Exercise01/.stack-work/install/x86_64-linux-tinfo6/3c68f6377a3187d2f8c1475b9b6fab501756f8d123a6e1be6d551a7060561537/8.10.7/libexec/x86_64-linux-ghc-8.10.7/ex01-0.1.0.0"
+sysconfdir = "/home/sauvagen/CS34016/Exercise01/.stack-work/install/x86_64-linux-tinfo6/3c68f6377a3187d2f8c1475b9b6fab501756f8d123a6e1be6d551a7060561537/8.10.7/etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "ex01_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "ex01_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "ex01_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "ex01_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "ex01_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "ex01_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
---
+++ Exercise01/src/Ex01.hs
@@ -0,0 +1,69 @@
+module Ex01 where
+import Data.Char (toLower)
+
+name, idno, username :: String
+name      =  "Sauvage,  Niall"  -- replace with your name
+idno      =  "20334203"    -- replace with your student id
+username  =  "sauvagen"   -- replace with your TCD username
+
+
+declaration -- do not modify this
+ = unlines
+     [ ""
+     , "@@@ This exercise is all my own work."
+     , "@@@ Signed: " ++ name
+     , "@@@ "++idno++" "++username
+     ]
+
+
+{- Part 1
+
+Write a function 'lower' that converts a string to lowercase
+
+Hint: 'toLower :: Char -> Char' converts a character to lowercase
+if it is uppercase. All other characters are unchanged.
+It is imported should you want to use it.
+
+-}
+lower :: String -> String
+lower [] = ""
+lower (x:xs) = toLower x
+
+
+{- Part 2
+
+Write a function 'nth' that returns the nth element of a list.
+Hint: the test will answer your Qs
+
+-}
+nth :: Int -> [a] -> a
+nth = undefined
+
+{- Part 3
+
+Write a function `commonPfx` that compares two sequences
+and returns the prefix they have in common.
+
+-}
+commonPfx :: Eq a => [a] -> [a] -> [a]
+commonPfx = undefined
+
+{- Part 4
+
+(TRICKY!) (VERY!)
+
+Write a function `runs` that converts a list of things
+into a list of sublists, each containing elements of the same value,
+which when concatenated together give the same list
+
+So `runs [1,2,2,1,3,3,3,2,2,1,1,4]`
+ becomes `[[1],[2,2],[1],[3,3,3],[2,2],[1,1],[4]]`
+
+Hint:  `elem :: Eq a => a -> [a] -> Bool`
+
+HINT: Don't worry about code efficiency
+       Seriously, don't!
+
+-}
+runs :: Eq a => [a] -> [[a]]
+runs = undefined
